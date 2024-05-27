9f60bd384049eff41d1d4b8ce5a91a1e22473db3
yuma-kasahara <yuma.kasahara@dena.com>
2024-05-27 14:00:53 +0900

    update: feの環境つくり直した

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import './main.scss';
-import App from './app/App';
-import { store } from './shared/store';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: 3000,
-    // proxy: {
-    //   "/hello": "http://localhost:9000",
-    //   "/api": "http://localhost:9000",
-    // },
-  },
   plugins: [react()],
 })
