34b75f9ad9ba59d124b20f87385e1569dfeb6b86
Takanori Horibe <takanori.horibe@dena.com>
2024-05-27 17:53:02 +0900

    [add] servers テーブルの作成 & insert データの作成

--- mysql/sql/insert.sql
+++ mysql/sql/insert.sql
@@ -7,5 +7,7 @@ INSERT INTO users (name, password) VALUES ('hanako', 'PASSWORD');
 INSERT INTO posts (user_id, title, body) VALUES (1, 'test1', '質問1\n改行');
 INSERT INTO posts (user_id, title, body) VALUES (1, 'test2', '質問2\n改行');
 
+INSERT INTO posts (name, owner, icon) VALUES ("server1", 'manager', 'hoge');
+
 INSERT INTO channels (server_id, name) VALUES (1, 'channel1');
 INSERT INTO channels (server_id, name) VALUES (1, 'channel2');
