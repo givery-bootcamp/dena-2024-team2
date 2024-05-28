7240ad92c62c8a9d541d6cec6fb0ab8ba8c26644
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-05-28 17:56:37 +0900

    #17 update: textの変更に伴った修正

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -4,7 +4,7 @@ import styles from "./header.module.scss";
 export const Header = () => {
 	return (
 		<header className={styles.header}>
-			<Text as="h1" size="xl">
+			<Text as="h1" size="xl" bold>
 				team-2 app
 			</Text>
 		</header>
