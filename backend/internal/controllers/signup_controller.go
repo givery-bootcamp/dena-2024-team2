d4098a6b49f0cd19efbb418a9021b49210eacc97
Takanori Horibe <takanori.horibe@gmail.com>
2024-06-17 14:49:24 +0900

    [add] validateSignUpParameters()

--- backend/internal/controllers/signup_controller.go
+++ backend/internal/controllers/signup_controller.go
@@ -1,7 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"fmt"
+	"myapp/internal/entities"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SignUp(ctx *gin.Context) {
 
+}
+
+// name or password のどちらかが不足のときエラーにする
+func validateSignUpParameters(user entities.User) error {
+	if user.Name == "" || user.Password == "" {
+		return errors.New(fmt.Sprintf("Missing name or password"))
+	}
+	return nil
 }
