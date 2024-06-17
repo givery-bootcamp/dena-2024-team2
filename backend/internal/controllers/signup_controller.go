d9279977550b9fb1a4cc025508700c09db6f29ac
Takanori Horibe <takanori.horibe@gmail.com>
2024-06-17 18:06:56 +0900

    [fix] lint の指摘を修正
    - uc.Execute() の err != nil の場合は後日追加する
    - 200 の場合は一旦、"success" を返す

--- backend/internal/controllers/signup_controller.go
+++ backend/internal/controllers/signup_controller.go
@@ -12,6 +12,10 @@ func SignUp(ctx *gin.Context) {
 
 	var account entities.Account
 	ctx.BindJSON(&account)
+	if err := ctx.BindJSON(&account); err != nil {
+		handleError(ctx, 400, err)
+		return
+	}
 	fmt.Println(account)
 	if err := validateSignUpParameters(account); err != nil {
 		handleError(ctx, 400, err)
@@ -19,8 +23,12 @@ func SignUp(ctx *gin.Context) {
 	}
 
 	usecase := usecases.NewSignUpUsecase()
-	usecase.Execute(ctx)
+	err := usecase.Execute(ctx)
+	if err != nil {
 
+	} else {
+		ctx.JSON(200, "suceess")
+	}
 }
 
 // name or password のどちらかが不足のときエラーにする
