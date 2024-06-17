59b8f959662b77d3584063c5b41342bfa82965bd
shogo-ozaki <shogo.ozaki@dena.com>
2024-06-17 18:42:45 +0900

    save

--- backend/internal/middleware/jwt.go
+++ backend/internal/middleware/jwt.go
@@ -1,5 +1,14 @@
 package middleware
 
-func GenerateToken(userId uint) {
+import (
+	"github.com/go-jose/go-jose/v3/jwt"
+	"github.com/golang-jwt/jwt"
+)
 
+func GenerateToken(userId uint) {
+	key := "secret"
+	t := jwt.NewWithClaims(jwt.SigningMethodES256,
+		jwt.MapClaims{
+			"iss": "hoge",
+		})
 }
