2c8db21acafc8611c5859c1eefaa408ff3e51489
Takanori Horibe <takanori.horibe@gmail.com>
2024-06-18 17:32:05 +0900

    [improve] Call EncryptPassword() in signup_usecase()

--- backend/internal/usecases/signup_usercase.go
+++ backend/internal/usecases/signup_usercase.go
@@ -1,6 +1,7 @@
 package usecases
 
 import (
+	"myapp/internal/infrastructures"
 	"myapp/internal/repositories"
 
 	"github.com/gin-gonic/gin"
@@ -14,8 +15,14 @@ func NewSignupUsecase() *SignupUsecase {
 }
 
 func (uc *SignupUsecase) Execute(ctx *gin.Context, name string, password string) error {
+
+	encryptPw, encryptErr := infrastructures.EncryptPassword(password)
+	if encryptErr != nil {
+		return encryptErr
+	}
+
 	repository := repositories.NewSignupRepository(DB(ctx))
-	err := repository.Signup(name, password)
+	err := repository.Signup(name, encryptPw)
 
 	return err
 }
