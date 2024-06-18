c52a6a2de6eb0153352969d1256d02a0b72f9ceb
Takanori Horibe <takanori.horibe@gmail.com>
2024-06-18 18:01:23 +0900

    [fix] JSON 変換を適用する

--- backend/internal/controllers/signup_controller.go
+++ backend/internal/controllers/signup_controller.go
@@ -2,16 +2,20 @@ package controllers
 
 import (
 	"fmt"
-	"myapp/internal/entities"
 	"myapp/internal/usecases"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type requestParam struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
 func Signup(ctx *gin.Context) {
 
-	var account entities.Account
+	var account requestParam
 	if err := ctx.BindJSON(&account); err != nil {
 		handleError(ctx, 400, err)
 		return
@@ -32,7 +36,7 @@ func Signup(ctx *gin.Context) {
 }
 
 // name or password のどちらかが不足のときエラーにする
-func validateSignupParameters(account entities.Account) error {
+func validateSignupParameters(account requestParam) error {
 	if account.Name == "" || account.Password == "" {
 		return fmt.Errorf("%v", "Missing name or password")
 	}
