645d6135b49c5047fb9fcbcd9f46dd28c53d7353
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-06-24 17:52:27 +0900

    add: tanstack queryを使う前準備

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,13 +2,14 @@ import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Header } from "~/components/header";
 import "modern-css-reset";
+import styles from "./__root.module.scss";
 
 export const Route = createRootRoute({
 	component: () => (
-		<>
+		<div className={styles.root}>
 			<Header />
 			<Outlet />
 			<TanStackRouterDevtools />
-		</>
+		</div>
 	),
 });
