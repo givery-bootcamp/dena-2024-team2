75d945abf1c01d494360386156f89356c943c928
Takanori Horibe <takanori.horibe@gmail.com>
2024-06-25 17:57:11 +0900

    [add] 存在しているサーバーか確認する

--- backend/internal/usecases/create_channel_usecase.go
+++ backend/internal/usecases/create_channel_usecase.go
@@ -14,6 +14,11 @@ func NewCreateChannelUsecase() *CreateChannelUsecase {
 }
 
 func (uc *CreateChannelUsecase) Execute(ctx *gin.Context, serverId int, name string) error {
+	// 存在しているサーバーか確認する
+	serverRepository := repositories.NewGetServerRepository(DB(ctx))
+	if _, err := serverRepository.Get(serverId); err != nil {
+		return nil, err
+	}
 
 	repository := repositories.NewCreateChannelRepository(DB(ctx))
 	err := repository.Post(serverId, name)
