d008b136f7dfbd93e7f5caa5fcde1c89422cf6ce
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-06-25 13:54:25 +0900

    update: apiClientにauth headerをデフォルトで追加（一次対応）

--- frontend/src/utils/api-client/api-client.ts
+++ frontend/src/utils/api-client/api-client.ts
@@ -1,3 +1,18 @@
 import ky from "ky";
 
-export const apiClient = ky.create({ prefixUrl: process.env.API_BASE_URL });
+export const apiClient = ky
+	.create({ prefixUrl: process.env.API_BASE_URL })
+	.extend({
+		hooks: {
+			beforeRequest: [
+				(request) => {
+					request.headers.set(
+						"Authorization",
+						// FIXME: 一時的に localStorage から取得するようにしている
+						// 本来はset-cookieするので、そうなったら修正する
+						`Bearer ${localStorage.getItem("TMP_TOKEN")}`,
+					);
+				},
+			],
+		},
+	});
