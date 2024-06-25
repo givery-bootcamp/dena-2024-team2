9119d830b61ccb78926441b2b085bd6d5e7a0ad3
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-06-25 16:55:16 +0900

    update: cors用の設定を追加

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -10,5 +10,16 @@ func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{myConfig.CorsAllowOrigin}
 	config.AllowCredentials = true
+	config.AllowMethods = []string{
+		"POST",
+		"GET",
+		"OPTIONS",
+	}
+	config.AllowHeaders = []string{
+		"Access-Control-Allow-Credentials",
+		"Access-Control-Allow-Headers",
+		"Content-Type",
+		"Authorization",
+	}
 	return cors.New(config)
 }
