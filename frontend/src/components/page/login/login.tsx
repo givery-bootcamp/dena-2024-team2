a8968389fe95fe0c34f3cbc9cd117873122570e3
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-06-26 13:34:50 +0900

    update: ログイン画面の作成

--- frontend/src/components/page/login/login.tsx
+++ frontend/src/components/page/login/login.tsx
@@ -1,21 +1,32 @@
-import { Button, Card } from "~/components/ui";
-import styles from "./login.module.css";
+import { Button, Card, Input } from "~/components/ui";
+import styles from "./login.module.scss";
 
 export const LoginPage = () => {
+	const handleClickLogin = () => {
+		console.log("Login");
+	};
+
 	return (
 		<div className={styles.root}>
-			<Card
-				header={<h1>Login</h1>}
-				body={
-					<div className={styles.card__body}>
-						<input type="text" placeholder="Username" />
-						<input type="password" placeholder="Password" />
-						<Button size="md" color="primary" variant="fill">
-							Login
-						</Button>
-					</div>
-				}
-			/>
+			<div className={styles.card}>
+				<Card
+					header={<h1>Login</h1>}
+					body={
+						<div className={styles.card__body}>
+							<Input type="text" placeholder="Username" />
+							<Input type="password" placeholder="Password" />
+							<Button
+								size="md"
+								color="primary"
+								variant="fill"
+								onClick={handleClickLogin}
+							>
+								Login
+							</Button>
+						</div>
+					}
+				/>
+			</div>
 		</div>
 	);
 };
