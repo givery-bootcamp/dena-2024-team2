498c19863c36d702ed736a2d8ba5ee8368ca80c3
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-06-26 17:11:50 +0900

    update: api-clientの後処理にkeyのconvert処理を追加

--- frontend/src/utils/api-client/api-client.ts
+++ frontend/src/utils/api-client/api-client.ts
@@ -1,4 +1,5 @@
 import ky from "ky";
+import { convertKeyCaseRecursive } from "../convert-key-recursive/convert-key-case-recursive";
 
 export const apiClient = ky
 	.create({ prefixUrl: import.meta.env.VITE_API_BASE_URL })
@@ -12,6 +13,19 @@ export const apiClient = ky
 						// 本来はset-cookieするので、そうなったら修正する
 						`Bearer ${localStorage.getItem("TMP_TOKEN")}`,
 					);
+				},
+			],
+			afterResponse: [
+				// camelCaseをsnake_caseに変換する処理
+				async (_request, _options, response) => {
+					if (
+						response.headers.get("content-type")?.includes("application/json")
+					) {
+						const jsonRes = await response.json();
+						const modifiedResponse = convertKeyCaseRecursive(jsonRes, "camel");
+						return new Response(JSON.stringify(modifiedResponse));
+					}
+					return response;
 				},
 			],
 		},
