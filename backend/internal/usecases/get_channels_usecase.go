6abb105287721abf3ed61aa2140bea6001a96730
Takanori Horibe <takanori.horibe@gmail.com>
2024-06-26 16:51:46 +0900

    [add] interface get_channels_repository の追加

--- backend/internal/usecases/get_channels_usecase.go
+++ backend/internal/usecases/get_channels_usecase.go
@@ -1,9 +1,17 @@
 package usecases
 
+import "myapp/internal/interfaces"
+
 type GetChannelsUsecase struct {
+	serverRepository      interfaces.ServerRepository
+	getChannelsRepository interfaces.GetChannelsRepository
 }
 
-func NewGetChannelsUsecase() {
+func NewGetChannelsUsecase(sr interfaces.ServerRepository, gcr interfaces.GetChannelsRepository) *GetChannelsUsecase {
+	return &GetChannelsUsecase{
+		serverRepository:      sr,
+		getChannelsRepository: gcr,
+	}
 }
 
 func (u *GetChannelsUsecase) Execute() {
