65c200d1402ae36a386ffe7054bf846a747c3b7f
shogo-ozaki <shogo.ozaki@dena.com>
2024-06-27 10:39:54 +0900

    fix

--- backend/internal/entities/server.go
+++ backend/internal/entities/server.go
@@ -8,11 +8,11 @@ type Servers struct {
 	Servers []Server `json:"servers"`
 }
 type Server struct {
-	Id        int        `json:"id"`
-	OwnerId   int        `json:"serverId"`
-	Name      string     `json:"name"`
-	Icon      string     `json:"icon"`
-	CreatedAt time.Time  `json:"createdAt"`
-	UpdatedAt time.Time  `json:"updatedAt"`
-	DeletedAt *time.Time `json:"deletedAt"`
+	Id        int       `json:"id"`
+	OwnerId   int       `json:"serverId"`
+	Name      string    `json:"name"`
+	Icon      string    `json:"icon"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt"`
 }
