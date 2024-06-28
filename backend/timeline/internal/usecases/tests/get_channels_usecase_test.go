87487c051791c69618b0b06d8ce7d842c378fa1d
shogo-ozaki <shogo.ozaki@dena.com>
2024-06-28 14:57:05 +0900

    add table driven test

--- backend/timeline/internal/usecases/tests/get_channels_usecase_test.go
+++ backend/timeline/internal/usecases/tests/get_channels_usecase_test.go
@@ -1,9 +1,11 @@
 package usecase_test
 
 import (
+	"fmt"
 	"myapp/internal/entities"
 	"myapp/internal/interfaces"
 	"myapp/internal/usecases"
+	"reflect"
 	"testing"
 )
 
@@ -25,29 +27,77 @@ func (m *getChannelsRepositoryMock) Get(serverId int) ([]entities.Channel, error
 	return m.FakeGet(serverId)
 }
 func Test_GetChannelUsecase_1(t *testing.T) {
-	serverRepo := &serverRepositoryMock{
-		FakeGet: func(serverId int) (*entities.Server, error) {
-			get := &entities.Server{Id: serverId, Name: "server1", OwnerId: 1, Icon: "icon"}
-			return get, nil
+	type serverFakes struct {
+		Get func(serverId int) (*entities.Server, error)
+	}
+	type getChannelsFakes struct {
+		Get func(serverId int) ([]entities.Channel, error)
+	}
+	type args struct {
+		serverId int
+	}
+
+	tests := []struct {
+		name             string
+		serverFakes      serverFakes
+		getChannelsFakes getChannelsFakes
+		args             args
+		want             []entities.Channel
+		wantErr          bool
+	}{
+		{"get successfully",
+			serverFakes{
+				func(serverId int) (*entities.Server, error) {
+					get := &entities.Server{Id: serverId, Name: "server1", OwnerId: 1, Icon: "icon"}
+					return get, nil
+				}},
+			getChannelsFakes{
+				func(serverId int) ([]entities.Channel, error) {
+					get := []entities.Channel{
+						{Id: 1, ServerId: serverId, Name: "channel1"},
+					}
+					return get, nil
+				},
+			},
+			args{1},
+			[]entities.Channel{
+				{Id: 1, ServerId: 1, Name: "channel1"},
+			},
+			false,
+		},
+		{"server is not found",
+			serverFakes{
+				func(serverId int) (*entities.Server, error) {
+					return nil, fmt.Errorf("err")
+				}},
+			getChannelsFakes{
+				func(serverId int) ([]entities.Channel, error) {
+					get := []entities.Channel{
+						{Id: 1, ServerId: serverId, Name: "channel1"},
+					}
+					return get, nil
+				},
+			},
+			args{1},
+			[]entities.Channel{},
+			true,
 		},
 	}
 
-	getChannelsRepo := &getChannelsRepositoryMock{
-		FakeGet: func(serverId int) ([]entities.Channel, error) {
-			get := []entities.Channel{
-				{Id: 1, ServerId: serverId, Name: "channel1"},
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sr := &serverRepositoryMock{}
+			sr.FakeGet = tt.serverFakes.Get
+			gcr := &getChannelsRepositoryMock{}
+			gcr.FakeGet = tt.getChannelsFakes.Get
+			uc := usecases.NewGetChannelsUsecase(sr, gcr)
+			got, err := uc.Execute(tt.args.serverId)
+			if (err != nil) && tt.wantErr {
+				return
 			}
-			return get, nil
-		},
-	}
-
-	uc := usecases.NewGetChannelsUsecase(serverRepo, getChannelsRepo)
-	got, err := uc.Execute(1)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(got) != 1 {
-		t.Errorf("usecase.Execute() should return length 1 array but got = %d length array", len(got))
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetChannelsUsecase.Execute() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
