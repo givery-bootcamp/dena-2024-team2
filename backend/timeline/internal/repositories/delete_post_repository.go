04eed63bb9c879a76566e0fce2f272068879bea7
shunsuke-nihei <shunsuke.nihei@dena.com>
2024-06-28 15:55:33 +0900

    コメントアウトしたコードを削除

--- backend/timeline/internal/repositories/delete_post_repository.go
+++ backend/timeline/internal/repositories/delete_post_repository.go
@@ -16,21 +16,6 @@ func NewDeletePostRepository(conn *gorm.DB) *DeletePostsRepository {
 	}
 }
 
-// type reqPost struct {
-// 	Id        int           `json:"id"`
-// 	ChannelId int           `json:"channel_id"`
-// 	UserID    int           `json:"user_id"`           // 外部キーを示すフィールドを追加
-// 	User      entities.User `gorm:"foreignKey:UserID"` // 外部キーの関係を定義
-// 	Content   string        `json:"content"`
-// 	CreatedAt time.Time     `json:"created_at"`
-// 	UpdatedAt time.Time     `json:"updated_at"`
-// 	DeletedAt *time.Time    `json:"deleted_at"`
-// }
-
-// func (reqPost) TableName() string {
-// 	return "posts"
-// }
-
 func (r *DeletePostsRepository) Delete(postId int) error {
 	if err := r.Conn.
 		Table("posts").
