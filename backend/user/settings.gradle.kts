a1cc7e85f9bd2bb5ba4d4ee0ada1f2dfb7275d97
toliner <seiya.tanaka@dena.com>
2024-06-28 17:43:08 +0900

    add usecase, domain, repository module

--- backend/user/settings.gradle.kts
+++ backend/user/settings.gradle.kts
@@ -2,3 +2,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "myapp-user"
+include("repository")
+include("usecase")
+include("domain")
---
+++ backend/user/usecase/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.ksp)
+}
+
+group = "myapp"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.bundles.koin)
+    ksp(libs.koin.ksp)
+    api(project(":domain"))
+    testImplementation(libs.test.kotlin)
+    testImplementation(libs.bundles.kotest)
+    testImplementation(libs.test.koin)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+configure<KtlintExtension> {
+    filter {
+        exclude("build.gradle.kts")
+        exclude("**/generated/**")
+    }
+}
