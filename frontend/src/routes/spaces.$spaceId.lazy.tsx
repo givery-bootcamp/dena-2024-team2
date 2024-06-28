6f121ba42b37e1c7982a5bd1903e0e58ea8e6632
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-06-28 13:41:01 +0900

    delete

--- frontend/src/routes/spaces.$spaceId.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-import { SpaceDetailPage } from "~/components/page";
-
-export const Route = createLazyFileRoute("/spaces/$spaceId")({
-	component: () => <SpaceDetailPage />,
-});
