50d2e7fd1301cca8fed64940e76c69816c4adb55
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-07-01 08:46:03 +0900

    #117 add

--- frontend/src/components/page/posts/components/index.ts
+++ frontend/src/components/page/posts/components/index.ts
@@ -1,3 +1,4 @@
 export { ChannelsPanel } from "./channels-panel";
 export { PostForm } from "./post-form/post-form";
 export { PostsPanel } from "./posts-panel/posts-panel";
+export { NewChannelDialog } from "./new-channel-dialog";
---
+++ frontend/src/components/page/posts/components/new-channel-dialog/index.ts
@@ -0,0 +1,1 @@
+export { NewChannelDialog } from "./new-channel-dialog";
