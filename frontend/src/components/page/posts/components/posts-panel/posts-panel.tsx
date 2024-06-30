1974734b9408c35527006837a80886177239c263
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-07-01 08:50:06 +0900

    #118 update

--- frontend/src/components/page/posts/components/posts-panel/posts-panel.tsx
+++ frontend/src/components/page/posts/components/posts-panel/posts-panel.tsx
@@ -1,12 +1,16 @@
 import { PostList } from "~/components/model/posts";
 import { Card, Text } from "~/components/ui";
 import type { Post } from "~/domains/posts";
+import { useScrollLatestPost } from "../../hooks";
+
 type Props = {
 	posts: Post[];
 	channelName: string;
 };
 
 export const PostsPanel = ({ posts, channelName }: Props) => {
+	const { boardRef } = useScrollLatestPost(posts);
+
 	return (
 		<Card
 			header={
@@ -15,6 +19,7 @@ export const PostsPanel = ({ posts, channelName }: Props) => {
 				</Text>
 			}
 			body={<PostList posts={posts} />}
+			bodyRef={boardRef}
 		/>
 	);
 };
